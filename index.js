1e5d3b9d06b835d319f990b5aac8ae1a1dc2555d
AliMukhtar99 <ali.mukhta@student.moringaschool.com>
2022-08-08 12:23:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names=["Ali", "Mukhtar", "Khidhr"], events = "birthday") {
+    let returnArray = [];
+    for(let i = 0; i < names.length; i++) {
+        returnArray[i] = `Thank you, ${names[i]}, for the wonderful ${events} gift!`
+    }
+    return returnArray;
+}
+console.log(writeCards())
+
+function countDown(int) {
+    while (int>= 0) {
+        console.log(int)
+        int--
+    }
+}
+countDown(10) 
